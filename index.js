cc254f37e0566b604254ca8ce3857b48da90cccc
jeroentvb <jeroentvb@hotmail.com>
2019-05-29 14:24:11 +0200

    Switch from request to node-fetch

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express"),
+  fetch = require("node-fetch"),
   request = require("request"),
   bodyParser = require("body-parser"),
   app = express(),
@@ -20,22 +21,27 @@ const express = require("express"),
   // .get("/", function );
 
   http.listen(port, () => {
-    console.log(port);
+    console.log(`[server] listening on port ${port}`);
   });
 
 function getData(id){
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const vidID = id || 1001004 ;
     const url = "https://openbeelden.nl/feeds/oai/?verb=GetRecord&identifier=oai:openimages.eu:" + vidID + "&metadataPrefix=oai_dc";
-    console.log(vidID);
-    console.log(url);
-    request(url, function(error, response, body){
-      let dataJson = JSON.parse(xmlParser.toJson(body));
-      console.log(dataJson);
-      resolve(dataJson);
-      reject(this.statusText);
-    })
+
+    try {
+      const res = await fetch(url);
+      const xml = await res.text();
+
+      const data = xmlParser.toJson(xml);
+
+      resolve(data);
+    } catch(err) {
+      reject(err);
+    }
   })
 }
 
-getData();
+getData()
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
