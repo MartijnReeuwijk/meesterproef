6077a78024f8ec0b948dfee4d4e314fd1c506490
Martijn <m.reeuwijk@Live.nl>
2019-06-03 15:48:08 +0200

    firstLoadNumberFunction

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express"),
   fetch = require("node-fetch"),
-  request = require("request"),
+  // request = require("request"),
   bodyParser = require("body-parser"),
   app = express(),
   http = require("http").Server(app),
   port = process.env.PORT || 5000,
   xmlParser = require("xml2json"),
-  fs = require("fs"),
-  Filehound = require("filehound"),
-  rp = require("request-promise");
+  fs = require("fs");
+  // Filehound = require("filehound"),
+  // rp = require("request-promise");
 
 app
   .set("view engine", "ejs")
@@ -26,11 +26,22 @@ http.listen(port, () => {
   console.log(port);
 });
 
-const firstLoadNumberArray = ["1000452","1000836","1000986","1001158","1001176","1001466","1001487","1001533","1001563","1001741","1001766","1001921","1002043","1002054","10021","1002986","1003237"]
+
+function firstLoadNumberFunction(){
+  let firstLoadNumberArray = ["1005255","1005236","1005201","1004470","1004205","1004044","1004013","1003891","1003714","1003690","1003648","1003627","1003511","1000452","1000836","1000986","1001158","1001176","1001466","1001487","1001533","1001563","1001741","1001766","1001921","1002043","1002054","10021","1002986","1003237"]
+  let homePageImagesArray = []
+  for (let i = 0; i < 9; i++) {
+    // let rand = firstLoadNumberArray[Math.floor(Math.random() * firstLoadNumberArray.length)];
+    homePageImagesArray.push(firstLoadNumberArray[Math.floor(Math.random() * firstLoadNumberArray.length)])
+  }
+return homePageImagesArray
+}
 
 function firstRandom(req, res) {
+  let clips = firstLoadNumberFunction()
+  console.log(clips);
   res.render("index.ejs", {
-    // clips: numberArray
+    clips: clips
   });
 }
 
