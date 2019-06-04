4a4f12b11ae987451184cc022bb4500e62727085
Martijn <m.reeuwijk@Live.nl>
2019-06-04 10:17:18 +0200

    Get 9 random images from the AI

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const express = require("express"),
   http = require("http").Server(app),
   port = process.env.PORT || 5000,
   xmlParser = require("xml2json"),
-  fs = require("fs");
-  // Filehound = require("filehound"),
+  fs = require("fs"),
+  DataArray = require('./static/array.js');
   // rp = require("request-promise");
 
 app
@@ -28,18 +28,17 @@ http.listen(port, () => {
 
 
 function firstLoadNumberFunction(){
-  let firstLoadNumberArray = ["1005255","1005236","1005201","1004470","1004205","1004044","1004013","1003891","1003714","1003690","1003648","1003627","1003511","1000452","1000836","1000986","1001158","1001176","1001466","1001487","1001533","1001563","1001741","1001766","1001921","1002043","1002054","10021","1002986","1003237"]
+  // let firstLoadNumberArray = ["1005255","1005236","1005201","1004470","1004205","1004044","1004013","1003891","1003714","1003690","1003648","1003627","1003511","1000452","1000836","1000986","1001158","1001176","1001466","1001487","1001533","1001563","1001741","1001766","1001921","1002043","1002054","10021","1002986","1003237"]
   let homePageImagesArray = []
   for (let i = 0; i < 9; i++) {
     // let rand = firstLoadNumberArray[Math.floor(Math.random() * firstLoadNumberArray.length)];
-    homePageImagesArray.push(firstLoadNumberArray[Math.floor(Math.random() * firstLoadNumberArray.length)])
+    homePageImagesArray.push(DataArray[Math.floor(Math.random() * DataArray.length)])
   }
 return homePageImagesArray
 }
 
-function firstRandom(req, res) {
+async function firstRandom(req, res) {
   let clips = firstLoadNumberFunction()
-  console.log(clips);
   res.render("index.ejs", {
     clips: clips
   });
