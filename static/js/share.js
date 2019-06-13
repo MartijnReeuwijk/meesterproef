e154a32fa478c240c9460f339ae0dec914091f21
Martijn <m.reeuwijk@Live.nl>
2019-06-13 15:35:28 +0200

    runnned buildded

--- static/js/share.js
+++ static/js/share.js
@@ -1,14 +1,13 @@
-function showOverlay() {
-  const overlay = document.getElementsByClassName('socialHolder')[0];
-  const shadow = document.getElementsByClassName('overlayDark')[0];
-  if (shadow.classList.contains("displayNone")) {
-    overlay.classList.remove("displayNone");
-    shadow.classList.remove("displayNone");
-  }
-  else {
-    overlay.classList.add("displayNone");
-    shadow.classList.add("displayNone");
+function showOverlay () {
+  const overlay = document.getElementsByClassName('socialHolder')[0]
+  const shadow = document.getElementsByClassName('overlayDark')[0]
+  if (shadow.classList.contains('displayNone')) {
+    overlay.classList.remove('displayNone')
+    shadow.classList.remove('displayNone')
+  } else {
+    overlay.classList.add('displayNone')
+    shadow.classList.add('displayNone')
   }
 }
-document.getElementsByClassName('overlayDark')[0].addEventListener("click",showOverlay)
-document.getElementsByClassName('closeOverlay')[0].addEventListener("click",showOverlay)
+document.getElementsByClassName('overlayDark')[0].addEventListener('click', showOverlay)
+document.getElementsByClassName('closeOverlay')[0].addEventListener('click', showOverlay)
