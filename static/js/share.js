0a38d0a347206c33f187f7235f0df0a8bdcb34dd
Martijn <m.reeuwijk@Live.nl>
2019-06-13 15:01:52 +0200

    overlay fix

---
+++ static/js/share.js
@@ -0,0 +1,14 @@
+function showOverlay() {
+  const overlay = document.getElementsByClassName('socialHolder')[0];
+  const shadow = document.getElementsByClassName('overlayDark')[0];
+  if (shadow.classList.contains("displayNone")) {
+    overlay.classList.remove("displayNone");
+    shadow.classList.remove("displayNone");
+  }
+  else {
+    overlay.classList.add("displayNone");
+    shadow.classList.add("displayNone");
+  }
+}
+document.getElementsByClassName('overlayDark')[0].addEventListener("click",showOverlay)
+document.getElementsByClassName('closeOverlay')[0].addEventListener("click",showOverlay)
