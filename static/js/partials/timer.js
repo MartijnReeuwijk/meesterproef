e8bb21b8871c3402943f4e7d8e1c002a94262ae5
Martijn <m.reeuwijk@Live.nl>
2019-06-18 15:51:43 +0200

    fixed the fade-in

--- static/js/partials/timer.js
+++ static/js/partials/timer.js
@@ -4,17 +4,29 @@ import { render } from './render.js'
 
 let timerVar
 
+function fade (time) {
+  setTimeout(() => {
+    const addFadeout = document.getElementsByClassName('mainpageImage');
+    const addFadeoutArray = Array.from(addFadeout)
+    addFadeoutArray.forEach(elem => {
+      elem.classList.add("fadeOut")
+    })
+  }, time)
+}
+
 function start (interval) {
+  fade(interval - 500)
+
   timerVar = setInterval(async () => {
     const url = window.location.href
     const urlParts = url.split('/')
     const images = urlParts[urlParts.length - 1].split('-')
     const currentImage = images[images.length - 1]
+    fade(interval - 500)
 
     if (url.includes('/search')) {
       try {
         const images = await data.related(currentImage)
-        console.log(images)
         await render.newImages(images)
 
         thumbnailsEventListener()
@@ -25,6 +37,8 @@ function start (interval) {
       try {
         const images = await data.random()
         await render.newImages(images)
+
+        fade(interval - 500)
 
         thumbnailsEventListener()
       } catch (err) {
@@ -43,6 +57,8 @@ function related (images, interval) {
     }
 
     await render.newImages(newImages)
+
+    fade(interval - 500)
 
     thumbnailsEventListener()
   }, interval)
