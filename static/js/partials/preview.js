dfdde4474de1d947fa9f081462e87b849071106e
Martijn <m.reeuwijk@Live.nl>
2019-06-24 10:26:50 +0200

    preview

---
+++ static/js/partials/preview.js
@@ -0,0 +1,57 @@
+function toggleDetailPage() {
+  document.getElementsByClassName('modalHolder')[0].classList.toggle('displayNone')
+  document.getElementsByClassName('shadowOverlay')[0].classList.toggle('displayNone')
+
+}
+document.getElementsByClassName('closeDetail')[0].addEventListener('click', toggleDetailPage)
+document.getElementsByClassName('shadowOverlay')[0].addEventListener('click', toggleDetailPage)
+
+const prev = Array.from(document.getElementsByClassName('prevButton'));
+prev.forEach(elem => {
+  // elem.addEventListener('click', toggleDetailPage)
+  elem.addEventListener('click', function() {
+    toggleDetailPage();
+    makeDetailPage(this);
+  });
+});
+
+// title
+// author
+// co
+// length
+// date
+// directed
+async function makeDetailPage(button) {
+const response = await getDetailData(button.dataset.image)
+const data = response.GetRecord.record.metadata["oai_dc:dc"]
+console.log(data);
+
+const newData = {
+  title: data["dc:title"].$t ? data["dc:title"].$t : data["dc:title"]["0"].$t,
+  co: "a",
+  data:data["dc:date"],
+  creator: data["dc:creator"].$t ? data["dc:creator"].$t : data["dc:creator"]["0"].$t
+}
+
+
+document.getElementsByClassName("title")[0].textContent = newData.title;
+document.getElementsByClassName("creator")[0].textContent = newData.creator;
+document.getElementsByClassName("date")[0].textContent = data["dc:date"];
+
+
+}
+
+
+async function getDetailData(video) {
+  const id = video
+  return new Promise(async function(resolve, reject) {
+    try {
+      const res = await fetch(`/ob-video/${id}`)
+      let data = await res.json()
+      resolve(data)
+    } catch (err) {
+      reject(err)
+    }
+  });
+}
+// /ob-video/:id
