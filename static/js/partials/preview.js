2aec191272eb79513ed22501b84b444e46586290
Martijn <m.reeuwijk@Live.nl>
2019-06-24 11:22:43 +0200

    previes video

--- static/js/partials/preview.js
+++ static/js/partials/preview.js
@@ -1,6 +1,7 @@
 function toggleDetailPage() {
   document.getElementsByClassName('modalHolder')[0].classList.toggle('displayNone')
   document.getElementsByClassName('shadowOverlay')[0].classList.toggle('displayNone')
+  document.getElementsByClassName("videoFile")[0].pause();
 
 }
 document.getElementsByClassName('closeDetail')[0].addEventListener('click', toggleDetailPage)
@@ -24,20 +25,17 @@ prev.forEach(elem => {
 async function makeDetailPage(button) {
 const response = await getDetailData(button.dataset.image)
 const data = response.GetRecord.record.metadata["oai_dc:dc"]
-console.log(data);
-
 const newData = {
   title: data["dc:title"].$t ? data["dc:title"].$t : data["dc:title"]["0"].$t,
   co: "a",
   data:data["dc:date"],
+  video: data["dc:format"][0],
   creator: data["dc:creator"].$t ? data["dc:creator"].$t : data["dc:creator"]["0"].$t
 }
-
-
 document.getElementsByClassName("title")[0].textContent = newData.title;
 document.getElementsByClassName("creator")[0].textContent = newData.creator;
-document.getElementsByClassName("date")[0].textContent = data["dc:date"];
-
+document.getElementsByClassName("date")[0].textContent = newData.data;
+document.getElementsByClassName("videoFile")[0].src = newData.video;
 
 }
 
