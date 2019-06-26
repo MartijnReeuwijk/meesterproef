df410b6c27ee4a783794409090abe50de3bb3ae8
Martijn <m.reeuwijk@Live.nl>
2019-06-26 12:30:07 +0200

    run build

--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,26 @@
-import { url } from "./partials/url.js";
-import { timer } from "./partials/timer.js";
+import { url } from './partials/url.js'
+import { timer } from './partials/timer.js'
 
-export function thumbnailsEventListener() {
-  let thumbnails = document.getElementsByClassName("previewImage");
+export function thumbnailsEventListener () {
+  let thumbnails = document.getElementsByClassName('previewImage')
 
   for (let i = 0; i < thumbnails.length; i++) {
-    thumbnails[i].addEventListener("click", e => {
-      let image = e.target.dataset.image;
+    thumbnails[i].addEventListener('click', e => {
+      let image = e.target.dataset.image
       // let shotNum = e.target.childNodes[1].dataset.shot
 
-      url.add(image);
-    });
+      url.add(image)
+    })
   }
 }
 
-function init() {
-  timer.start(5000);
-  thumbnailsEventListener();
-  if (document.querySelector(".history-container")) {
-    document.querySelector(".history-container").scrollTop = 0;
+function init () {
+  timer.start(5000)
+  thumbnailsEventListener()
+  if (document.querySelector('.history-container')) {
+    document.querySelector('.history-container').scrollTop = 0
   }
 }
-if (!window.location.href.includes("/end")) {
-  init();
+if (!window.location.href.includes('/end')) {
+  init()
 }
